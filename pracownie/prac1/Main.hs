bb2e52e609995d9af950205b995f4b9cc18d6d5d
Krzysztof Skrzętnicki <gtener@gmail.com>
2011-09-28 21:50:43 +0200

    udp and cli servers in place, protocol implementation pending

--- pracownie/prac1/Main.hs
+++ pracownie/prac1/Main.hs
@@ -1,12 +1,23 @@
-{-# LANGUAGE OverloadedStrings, ViewPatterns #-}
+{-# LANGUAGE OverloadedStrings, ViewPatterns, DeriveDataTypeable #-}
 
 module Main where
 
-import Network.Socket hiding (send, sendTo, recv, recvFrom)
-import Network.Socket.ByteString
+-- data-timeout
+import Control.Concurrent.Timeout as Timeout
+import Data.Timeout as Timeout
+-- blaze-builder
 import Blaze.ByteString.Builder
 import Blaze.ByteString.Builder.Char.Utf8
+-- cmdargs
 import System.Console.CmdArgs
+-- time
+import Data.Time
+-- network
+import Network hiding (sendTo, recvFrom)
+import qualified Network
+import Network.Socket hiding (send, sendTo, recv, recvFrom)
+import Network.Socket.ByteString
+-- base etc.
 import Data.Function
 import Data.Map (Map)
 import qualified Data.Map as Map
@@ -15,35 +26,46 @@ import qualified Data.Set as Set
 import Data.Word
 import Data.Monoid
 import Control.Concurrent.STM
+import Control.Concurrent
+import System.Environment
+import System.FilePath
+import System.IO as IO
 
-data Config = Config { helloInterval :: Double
-                     , address :: String
-                     , port :: Int
-                     , dieTime :: Double
+type Host = HostName
+type Time = UTCTime
+
+data Config = Config { helloInterval :: Int -- ^ the length of hello interval - the time (in seconds) between sending our interests to others
+                     , mcastAddress :: Host -- ^ multicast address
+                     , port :: Int -- ^ port to use
+                     , dieTime :: Int -- ^ number of intervals before the host is killed by inactivity
+                     , quiet :: Bool -- ^ should the deamon be quiet? true by default
+                     , localSocketPath :: FilePath -- ^ path to local control socket
                      }
+            deriving (Data, Typeable)
 
-data OtherSystem = OSyst { lastHeard :: Time -- ^ last time the system was heard from
-                         , interestedIn :: Interests -- ^ lists of items this system is interested in
-                         , timeoutThread :: ThreadID -- ^ thread that will kill this entry upon timeout
-                         }
+data OtherHost = Other { lastHeard :: Time -- ^ last time the host was heard from
+                       , interestedIn :: Interests -- ^ lists of items this system is interested in
+                       , timeoutThread :: ThreadId -- ^ thread that will kill this entry upon timeout
+                       }
 
 instance Default Config where
-    def = fix (\ self -> Config { helloInterval = 30
-                                , address = "224.0.0.213"
-                                , port = 6969
-                                , dieTime = 6 * (helloInterval self) })
+    def = Config { helloInterval = 30
+                 , mcastAddress = "224.0.0.213"
+                 , port = 6969
+                 , dieTime = 6
+                 , quiet = True
+                 , localSocketPath = "/tmp/pwz_tener.sock"
+                 }
 
 type Interest = [Word8]
 type Interests = Set Interest
-
-type RevocationList = PriorityQueue Time IpAddr
-type OtherSystems = Map IpAddr OtherSystem
+type OtherHosts = Map Host OtherHost
 
 -- | state of a running daemon.
 data DaemonState = DState { configuration :: Config -- ^ current configuration
-                          , conversants :: TVar OtherSystems -- ^ list of systems that have send their interests. will be mutated by 
+                          , conversants :: TVar OtherHosts -- ^ list of systems that have send their interests. will be mutated by 
                                                              --   other threads, hence 'TVar'.
-                          , myInterests :: Interests -- ^ our own interests
+                          , myInterests :: TVar Interests -- ^ our own interests
                           }
 
 serializeInterests :: Interests -> Builder
@@ -51,9 +73,75 @@ serializeInterests (Set.toList -> ints) =
   fromInt32be (fromIntegral (length ints)) `mappend`
   (mconcat $ map (\ i -> fromWord8s i `mappend` fromWord8 0 ) ints)
 
-updateFromSystem :: IpAddr -> Time -> Interests -> IO ()
-updateFromSystem addr time ints = 
+runDaemon :: IO ()
+runDaemon = do
+  config <- cmdArgs def
+  conv <- newTVarIO Map.empty
+  myInt <- newTVarIO Set.empty 
+  let _dstate = DState config conv myInt
 
-main = return ()
+  let -- UDP server
+      server_udp = do
+              sock <- socket AF_INET Datagram defaultProtocol -- (mcastAddress config)
+              addr <- inet_addr (mcastAddress config)
+              let socketAddress = SockAddrInet (fromIntegral (port config)) addr
+              let -- UDP receiver thread
+                  go_rec = do
+                        (payload, sender) <- recvFrom sock 65507
+                        -- try parse, update record
+                        print ("received",payload,"from",sender)
+                        go_rec
+                  -- UDP sender thread
+                  go_send = do
+                        Timeout.threadDelay (fromIntegral (helloInterval config) Timeout.# Second)
+                        sendInterestsNow
+                        go_send
+                  -- send current interests NOW
+                  sendInterestsNow = do
+                     int <- readTVarIO myInt
+                     sendAllTo sock (toByteString $ serializeInterests int) socketAddress
+
+              bindSocket sock socketAddress
+              forkIO go_rec
+              forkIO go_send
+
+      -- AF_UNIX server for CLI
+      server_cli = do
+            -- sock <- socket AF_UNIX Stream defaultProtocol
+            -- let socketAddress = SockAddrUnix (localSocketPath config)
+            -- bindSocket sock socketAddress
+            sock <- listenOn (UnixSocket (localSocketPath config))
+            let loop = do
+                  (conn, _, _) <- Network.accept sock
+                  cont <- IO.hGetContents conn
+                  print ("received",cont)
+                  hClose conn
+                  loop
+            forkIO loop
+            return ()
+
+  server_udp
+  server_cli
+
+  Timeout.threadDelay (1 Timeout.# Day)
+
+updateFromSystem :: Host -> Interests -> IO ()
+updateFromSystem addr ints = do
+  now <- getCurrentTime
+--  atomically $ do
+--    conversants
+
+  return ()
+  
+runCLI = do
+  Network.sendTo ""  (UnixSocket (localSocketPath def)) "foo"
+ 
+
+main = withSocketsDo $ do
+   prog <- getProgName
+   case prog of
+     "pwzd" -> runDaemon
+     "pwz" -> runCLI
+
 
 
